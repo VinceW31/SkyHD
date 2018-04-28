54b8ee1936af8443b7a37a102e2270a153a8e054
Vince Williams <vince.williams@goldstar31.com>
2018-04-28 16:19:19 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -28,78 +28,31 @@ echo ""
 read -r -p "Enter your SkyHD box IP address (format = 192.168.xxx.xxx): " skyboxip
 echo ""
 
-
-# modelid=$projid-$(date +%Y%m%d%H%M%S )
-# echo "Your Model-Id used for the project is: $modelid" >> /home/pi/modelid.txt
 cd /home/pi/
-#--------------SkyHD Dependencies------------------------------------------------
-# sudo apt-get install libxml2-dev libxslt-dev python-dev -y
-# sudo apt-get install mpv -y
-# mkdir -p /home/pi/.config/mpv/scripts/
-# mv /home/pi/GassistPi/src/end.lua /home/pi/.config/mpv/scripts/end.lua
-# sudo apt-get install mplayer -y
-# sudo pip3 install mps-youtube youtube-dl
-# sudo apt-get install vlc -y
-# mpsyt set player vlc, set playerargs ,exit
-# sudo apt-get install elinks -y
-#--------------------------------------------------------------------------------
+
 echo ""
 echo "Installing Raspbian updates....."
 echo ""
 sudo apt-get update -y
+
 echo ""
 echo "Installing nodejs npm.......(approx 5 mins)"
 echo ""
 sudo apt-get install nodejs npm node-semver -y
+
 echo ""
 echo "Installing sky-remote-cli....."
 echo ""
 sudo npm install -g sky-remote-cli -y
+
 cd /home/pi/SkyHD
 echo ""
 echo "Installing Broadlink BlackBean requirements.....(approx 5 mins)"
 echo ""
-
-
 pip install -r blackbean_requirements.txt
 
-
-
-
 cd /home/pi/SkyHD
 
-
-
-#sudo apt-get install python3-dev python3-venv -y
-#--------------GassistPi Deps----------------------------------------------------
-#sudo apt-get install portaudio19-dev libffi-dev libssl-dev -y
-#sudo apt-get install libttspico0 libttspico-utils libttspico-data -y
-#--------------------------------------------------------------------------------
-#python3 -m venv env
-#env/bin/python -m pip install --upgrade pip setuptools
-#source env/bin/activate
-#pip install wheel
-#pip install RPi.GPIO
-#--------------GassistPi Deps----------------------------------------------------
-#pip install pyaudio
-#pip install aftership
-#pip install feedparser
-#pip install kodi-json
-#pip install gmusicapi
-#pip install requests
-#pip install urllib3
-#pip install --upgrade google-api-python-client
-#--------------------------------------------------------------------------------
-#python -m pip install --upgrade google-assistant-library
-#python -m pip install --upgrade google-assistant-sdk
-#python -m pip install --upgrade google-assistant-sdk[samples]
-#python -m pip install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
-#google-oauthlib-tool --client-secrets /home/pi/$credname --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
-#googlesamples-assistant-devicetool register-model --manufacturer "Pi Foundation" \
-#          --product-name $prodname --type LIGHT --model $modelid
-#echo "Testing the installed google assistant. Make a note of the generated Device-Id"
-#googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
-cd /home/pi/SkyHD
 echo ""
 echo "This is the end of the installation script"
 echo ""
