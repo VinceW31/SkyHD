3f1481604b2da6d9d5e1e9918687a3f1828d8275
Vince Williams <vince.williams@goldstar31.com>
2018-05-02 13:48:02 +0100

    Update GetSkyIP.py

--- GetSkyIP.py
+++ GetSkyIP.py
@@ -1,4 +1,4 @@
 SKY = input ("Please enter your Sky Box IP address (format 192.168.xxx.xxx): ")
-data = ["[General]","skyboxip = " + SKY]
+data = ["[General]","skyboxip = " + str(SKY)]
 with open("skybox_ip.py", "w") as f:
     f.write("\n".join(data))
