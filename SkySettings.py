610dc993ad4889c0598b587ae81c294ee2598ba2
Vince Williams <vince.williams@goldstar31.com>
2018-05-02 11:29:13 +0100

    Update SkySettings.py

--- SkySettings.py
+++ SkySettings.py
@@ -2,7 +2,7 @@ import configparser
 from os import path
 
 ApplicationDir = path.dirname(path.abspath(__file__))
-SkyIPsettings =path.join(ApplicationDir, 'skybox_ip.ini')
+SkyIPSettings =path.join(ApplicationDir, 'skybox_ip.ini')
 
 Settings = configparser.ConfigParser()
 Settings.read(SkyIPSettings)
