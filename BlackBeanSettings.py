29e1444ed92aeaf427416fe4072f1b70beb8d5aa
Vince Williams <vince.williams@goldstar31.com>
2018-05-02 13:22:49 +0100

    Update BlackBeanSettings.py

--- BlackBeanSettings.py
+++ BlackBeanSettings.py
@@ -3,16 +3,12 @@ from os import path
 
 ApplicationDir = path.dirname(path.abspath(__file__))
 BlackBeanControlSettings = path.join(ApplicationDir, 'BlackBeanControl.ini')
-#SkyIPsettings =path.join(ApplicationDir, 'skybox_ip.ini')
 
 Settings = configparser.ConfigParser()
 Settings.read(BlackBeanControlSettings)
-#SkySettings = configparser.ConfigParser()
-#Settings2.read(SkySettings)
-
 
 IPAddress = Settings.get('General', 'IPAddress')
 Port = Settings.get('General', 'Port')
 MACAddress = Settings.get('General', 'MACAddress')
 Timeout = Settings.get('General', 'Timeout')
-#SkyIP = Settings2.get('General', 'skyboxip')
+
