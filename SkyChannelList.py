ac5796e3592fa4e3325832032981fa520d7edb73
Vince Williams <vince.williams@goldstar31.com>
2018-05-03 13:21:55 +0000

    Update SkyChannelList.py

--- SkyChannelList.py
+++ SkyChannelList.py
@@ -1,1 +1,75 @@
 SkyNews = 501
+Discovery = 125
+Gold = 110
+Dave = 111
+TLC = 140
+Alibi = 132
+Sky_Sports_F1 = 406
+Sky_Atlantic = 108
+Universal = 113
+National_Geographic = 129
+Channel_5 = 105
+Channel_4 = 138
+BBC_1 = 115
+BBC_2 = 102
+BBC_4 = 116
+BBC_Local = 101
+ITV_1 = 103
+ITV_2 = 118
+ITV_3 = 119
+ITV_4 = 120
+Sky_1 = 106
+
+
+
+
+SkyNews = str(SkyNews)
+Discovery = str(Discovery)
+Gold = str(Gold)
+Dave = str(Dave)
+TLC = str(TLC)
+Alibi = str(Alibi)
+Sky_Sports_F1 = str(Sky_Sports_F1)
+Sky_Atlantic = str(Sky_Atlantic)
+Universal = str(Universal)
+National_Geographic = str(National_Geographic)
+Channel_5 = str(Channel_5)
+Channel_4 = str(Channel_4)
+BBC_1 = str(BBC_1)
+BBC_2 = str(BBC_2)
+BBC_4 = str(BBC_4)
+BBC_Local = str(BBC_Local)
+ITV_1 = str(ITV_1)
+ITV_2 = str(ITV_2)
+ITV_3 = str(ITV_3)
+ITV_4 = str(ITV_4)
+Sky_1 = str(Sky_1)
+
+
+skynews = SkyNews[0] + " " + SkyNews[1] + " " + SkyNews[2]
+discovery = Discovery[0] + " " + Discovery[1] + " " + Discovery[2]
+gold = Gold[0] + " " + Gold[1] + " " + Gold[2]
+dave = Dave[0] + " " + Dave[1] + " " + Dave[2]
+tlc = TLC[0] + " " + TLC[1] + " " + TLC[2]
+alibi = Alibi[0] + " " + Alibi[1] + " " + Alibi[2]
+f1 = Sky_Sports_F1[0] + " " + Sky_Sports_F1[1] + " " + Sky_Sports_F1[2]
+atlantic = Sky_Atlantic[0] + " " + Sky_Atlantic[1] + " " + Sky_Atlantic[2]
+universal = Universal[0] + " " + Universal[1] + " " + Universal[2]
+nat_geo = National_Geographic[0] + " " + National_Geographic[1] + " " + National_Geographic[2]
+ch_5 = Channel_5[0] + " " + Channel_5[1] + " " + Channel_5[2]
+ch_4 = Channel_4[0] + " " + Channel_4[1] + " " + Channel_4[2]
+bbc_1 = BBC_1[0] + " " + BBC_1[1] + " " + BBC_1[2]
+bbc_2 = BBC_2[0] + " " + BBC_2[1] + " " + BBC_2[2]
+bbc_4 = BBC_4[0] + " " + BBC_4[1] + " " + BBC_4[2]
+bbc_local = BBC_Local[0] + " " + BBC_Local[1] + " " + BBC_Local[2]
+itv_1 = ITV_1[0] + " " + ITV_1[1] + " " + ITV_1[2]
+itv_2 = ITV_2[0] + " " + ITV_2[1] + " " + ITV_2[2]
+itv_3 = ITV_3[0] + " " + ITV_3[1] + " " + ITV_3[2]
+itv_4 = ITV_4[0] + " " + ITV_4[1] + " " + ITV_4[2]
+sky_1 = Sky_1[0] + " " + Sky_1[1] + " " + Sky_1[2]
+
+
+
+
+
+
