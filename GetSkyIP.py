32c9bbd1905a4e9116f2f92c2423ff2ca32ef4a5
Vince Williams <vince.williams@goldstar31.com>
2018-05-03 14:01:02 +0000

    Update GetSkyIP.py

--- GetSkyIP.py
+++ GetSkyIP.py
@@ -1,4 +1,22 @@
-SKY = input ("Please enter your Sky Box IP address (format 192.168.xxx.xxx): ")
-data = ["[General]","skyboxip = " + str(SKY)]
+
+sky = input ("Please enter your Sky Box IP address (format 192.168.xxx.xxx): ")
+   
+ip1 = sky.split('.',3)[0]
+ip2 = sky.split('.',3)[1]
+ip3 = sky.split('.',3)[2]
+ip4 = sky.split('.',3)[3]
+
+#print(ip1,ip2,ip3,ip4)
+#print(ip1)
+#print(ip2)
+#print(ip3)
+#print(ip4)
+
 with open("skybox_ip.py", "w") as f:
-    f.write("\n".join(data))
+    f.write("ip1 = " + ip1)
+    f.write("\n")
+    f.write("ip2 = " + ip2)
+    f.write("\n")
+    f.write("ip3 = " + ip3)
+    f.write("\n")
+    f.write("ip4 = " + ip4)
