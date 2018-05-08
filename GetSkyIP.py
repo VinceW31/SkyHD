d2ecbb1eaf62073c2ffe10845267e69d47f1da30
Vince Williams <vince.williams@goldstar31.com>
2018-05-08 13:56:26 +0100

    Update GetSkyIP.py

--- GetSkyIP.py
+++ GetSkyIP.py
@@ -1,22 +1,50 @@
 
-sky = input ("Please enter your Sky Box IP address (format 192.168.xxx.xxx): ")
+
+
+get_input()
+check_valid()
+Print_IP()
+Write_to_file()
+
+
+def retry():
+   get_input()
+   check_valid()
+   Print_IP()
+   Write_to_file()
    
-ip1 = sky.split('.',3)[0]
-ip2 = sky.split('.',3)[1]
-ip3 = sky.split('.',3)[2]
-ip4 = sky.split('.',3)[3]
+def check_valid():
+   try:
+      ip_split()
+   except:
+      check_reply = ("Invalid format, do you want to try again? (Y/N)")
+      if check_reply == "y":
+         retry()
+      else:
+         print("You must have a vaild IP address for your SkyHD Box to proceed")
+   
+def get_input():
+   sky = input ("Please enter your SkyHD Box IP address (format 192.168.xxx.xxx): ")
 
-#print(ip1,ip2,ip3,ip4)
-#print(ip1)
-#print(ip2)
-#print(ip3)
-#print(ip4)
-
-with open("skybox_ip.py", "w") as f:
-    f.write("ip1 = " + ip1)
-    f.write("\n")
-    f.write("ip2 = " + ip2)
-    f.write("\n")
-    f.write("ip3 = " + ip3)
-    f.write("\n")
-    f.write("ip4 = " + ip4)
+def ip_split(): 
+   ip1 = sky.split('.',3)[0]
+   ip2 = sky.split('.',3)[1]
+   ip3 = sky.split('.',3)[2]
+   ip4 = sky.split('.',3)[3]
+   
+def Print_IP():
+   print(ip1,ip2,ip3,ip4)
+   print(ip1)
+   print(ip2)
+   print(ip3)
+   print(ip4)
+   
+def write_to_file():
+   with open("skybox_ip.py", "w") as f:
+      f.write("ip1 = " + ip1)
+      f.write("\n")
+      f.write("ip2 = " + ip2)
+      f.write("\n")
+      f.write("ip3 = " + ip3)
+      f.write("\n")
+      f.write("ip4 = " + ip4)
