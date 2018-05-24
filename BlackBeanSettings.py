b477e6ffcdf619edeb50d839752ec34f4fbf3a0b
Vince Williams <vince.williams@goldstar31.com>
2018-05-24 14:55:30 +0100

    Update BlackBeanSettings.py

--- BlackBeanSettings.py
+++ BlackBeanSettings.py
@@ -1,1 +1,15 @@
+import configparser
+from os import path
+
+ApplicationDir = path.dirname(path.abspath(__file__))
+BlackBeanControlSettings = path.join(ApplicationDir, 'BlackBeanControl.ini')
+
+#Settings = ConfigParser.ConfigParser()
+Settings = configparser.ConfigParser()
+Settings.read(BlackBeanControlSettings)
+
+IPAddress = Settings.get('General', 'IPAddress')
+Port = Settings.get('General', 'Port')
+MACAddress = Settings.get('General', 'MACAddress')
+Timeout = Settings.get('General', 'Timeout')
 
